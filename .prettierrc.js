ea522273808902e295c20eb5aabae69218764251
MorganHuegel <mhue2910@gmail.com>
2019-09-07 14:44:34 -0400

    updated linter settings

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: false,
-  singleQuote: false,
-  trailingComma: 'none',
-};
+// module.exports = {
+//   bracketSpacing: false,
+//   jsxBracketSameLine: false,
+//   singleQuote: false,
+//   trailingComma: 'none',
+// };
