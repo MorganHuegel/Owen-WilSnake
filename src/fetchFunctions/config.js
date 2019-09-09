5866e9f63c0ac68d76a4475caf2459cf100be3be
MorganHuegel <mhue2910@gmail.com>
2019-09-08 23:03:52 -0400

    updated config file for fetch functions with live server_base_url

--- src/fetchFunctions/config.js
+++ src/fetchFunctions/config.js
@@ -1,3 +1,11 @@
+//Should use .env here, but I would have to figure out how to
+// use built-in Node modules first, like fs and path, which react-native
+// doesn't support??
+//const { SERVER_BASE_URL } = require('react-native-dotenv')
+
+const SERVER_BASE_URL = 'https://owen-wilsnake-server.herokuapp.com/'
+// const SERVER_BASE_URL = 'http://192.168.0.4:8080'
+
 module.exports = {
-  SERVER_BASE_URL: process.env.SERVER_BASE_URL || 'http://192.168.0.4:8080'
+  SERVER_BASE_URL: SERVER_BASE_URL || 'http://192.168.0.4:8080'
 };
