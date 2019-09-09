5500ec8644a8551013ed6339eaa4afcad107977f
MorganHuegel <mhue2910@gmail.com>
2019-09-08 21:13:30 -0400

    added .env setup

--- src/fetchFunctions/config.js
+++ src/fetchFunctions/config.js
@@ -1,3 +1,5 @@
+const { SERVER_BASE_URL } = require('react-native-dotenv')
+
 module.exports = {
-  SERVER_BASE_URL: process.env.SERVER_BASE_URL || 'http://192.168.0.4:8080'
+  SERVER_BASE_URL: SERVER_BASE_URL || 'http://192.168.0.4:8080'
 };
