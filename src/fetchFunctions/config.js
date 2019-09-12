73447ba278b6a1997e00d68745f4284ad4b5fdbf
MorganHuegel <mhue2910@gmail.com>
2019-09-12 13:11:10 -0400

    added third config for localhost server_base_url

--- src/fetchFunctions/config.js
+++ src/fetchFunctions/config.js
@@ -5,6 +5,7 @@
 
 const SERVER_BASE_URL = 'https://owen-wilsnake-server.herokuapp.com'
 // const SERVER_BASE_URL = 'http://192.168.0.4:8080'
+// const SERVER_BASE_URL = 'http://localhost:8080'
 
 module.exports = {
   SERVER_BASE_URL: SERVER_BASE_URL || 'http://192.168.0.4:8080'
