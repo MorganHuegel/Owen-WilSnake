0ee6540c6762308c61a436ee7ea0625c42dfa661
MorganHuegel <mhue2910@gmail.com>
2019-11-14 16:13:07 -0500

    updated OwenEatsChicken listener to accommodate varying item and face dimensions

--- src/components/gameplay/map/ChickenWing.js
+++ src/components/gameplay/map/ChickenWing.js
@@ -5,16 +5,13 @@ import { Image } from 'react-native';
 export function ChickenWing(props){
   const chickenWingStyles = {
     image: {
+      width: props.itemDimensions.itemWidth,
+      height: props.itemDimensions.itemHeight,
       position: 'absolute',
       left: props.chickenPosition.left,
       top: props.chickenPosition.top
     }
   }
 
-  const sizeStyles = {
-    width: props.itemDimensions.itemWidth,
-    height: props.itemDimensions.itemHeight
-  }
-
-  return <Image source={props.character.itemToEat} style={[chickenWingStyles.image, sizeStyles]}/> 
+  return <Image source={props.character.itemToEat} style={chickenWingStyles.image}/> 
 }
--- src/components/gameplay/map/owenSnake/OwenSnakeMainUtils.js
+++ src/components/gameplay/map/owenSnake/OwenSnakeMainUtils.js
@@ -2,10 +2,10 @@
 export function checkForDeath(){
   // if lead Owen hits the wall, DEAD!!
   if (
-    this.state.snakeBody[0].left.__getValue() === 0 || 
-    this.state.snakeBody[0].top.__getValue() === 0 ||
-    this.state.snakeBody[0].left.__getValue() === this.props.mapDimensions.width - this.props.cellDimensions.width ||
-    this.state.snakeBody[0].top.__getValue() === this.props.mapDimensions.height - this.props.cellDimensions.height
+    this.state.snakeBody[0].left.__getValue() <= 0 || 
+    this.state.snakeBody[0].top.__getValue() <= 0 ||
+    this.state.snakeBody[0].left.__getValue() >= this.props.mapDimensions.width - this.props.characterDimensions.characterWidth ||
+    this.state.snakeBody[0].top.__getValue() >= this.props.mapDimensions.height - this.props.characterDimensions.characterHeight
     ) {
     this.owenDies()
   }
@@ -13,8 +13,8 @@ export function checkForDeath(){
   // if lead Owen hits another owen, while traveling perpindicularly to that Owen, DEAD!!
   else if (this.state.snakeBody.find( (face, i) => {
     return (i > 3) &&
-      ( Math.abs(this.state.snakeBody[0].left.__getValue() - face.left.__getValue()) < this.props.cellDimensions.width ) &&
-      ( Math.abs(this.state.snakeBody[0].top.__getValue() - face.top.__getValue()) < this.props.cellDimensions.height ) &&
+      ( Math.abs(this.state.snakeBody[0].left.__getValue() - face.left.__getValue()) < this.props.characterDimensions.characterWidth ) &&
+      ( Math.abs(this.state.snakeBody[0].top.__getValue() - face.top.__getValue()) < this.props.characterDimensions.characterHeight ) &&
       ( 
         (this.state.snakeBody[0].moving === 'left'  || this.state.snakeBody[0].moving === 'right') ? 
         (face.moving === 'up' || face.moving === 'down') : 
@@ -26,16 +26,49 @@ export function checkForDeath(){
 }
 
 export function checkForChicken(){
-  // let itemWidth, itemHeight;
-  // if (this.props.character.itemWidth > this.props.character.itemHeight) {
-  //   itemWidth = 
-  // } else {
-
-  // }
-  if (
-    (Math.abs(this.state.snakeBody[0].left.__getValue() - this.props.chickenWing.left) < this.props.cellDimensions.width)
-    && (Math.abs(this.state.snakeBody[0].top.__getValue() - this.props.chickenWing.top) < this.props.cellDimensions.height)
+  // If the face is on the right and bottom of item
+  if ( 
+    (this.state.snakeBody[0].left.__getValue() > this.props.chickenWing.left) &&
+    (this.state.snakeBody[0].top.__getValue() > this.props.chickenWing.top)
   ) {
-    this.owenEatsChicken()
+    if (
+      (this.state.snakeBody[0].left.__getValue() < this.props.chickenWing.left + this.props.itemDimensions.itemWidth) &&
+      (this.state.snakeBody[0].top.__getValue() < this.props.chickenWing.top + this.props.itemDimensions.itemHeight)
+    ) {
+      this.owenEatsChicken()
+    }
+  }
+  // Else if the face is on the left bottom relative to item
+  else if (
+    !(this.state.snakeBody[0].left.__getValue() > this.props.chickenWing.left) &&
+    (this.state.snakeBody[0].top.__getValue() > this.props.chickenWing.top)
+  ) {
+    if (
+      (this.state.snakeBody[0].left.__getValue() + this.props.characterDimensions.characterWidth > this.props.chickenWing.left) &&
+      (this.state.snakeBody[0].top.__getValue() < this.props.chickenWing.top + this.props.itemDimensions.itemHeight)
+    ) {
+      this.owenEatsChicken()
+    }
+  }
+  // Else if the face is on top right relative to item
+  else if (
+    (this.state.snakeBody[0].left.__getValue() > this.props.chickenWing.left) &&
+    !(this.state.snakeBody[0].top.__getValue() > this.props.chickenWing.top)
+  ) {
+    if (
+      (this.state.snakeBody[0].left.__getValue() < this.props.chickenWing.left + this.props.itemDimensions.itemWidth) &&
+      (this.state.snakeBody[0].top.__getValue() + this.props.characterDimensions.characterHeight > this.props.chickenWing.top)
+    ) {
+      this.owenEatsChicken()
+    }
+  }
+  // Else if the face is on the top left side of item
+  else {
+    if (
+      (this.state.snakeBody[0].left.__getValue() + this.props.characterDimensions.characterWidth > this.props.chickenWing.left) &&
+      (this.state.snakeBody[0].top.__getValue() + this.props.characterDimensions.characterHeight > this.props.chickenWing.top)
+    ) {
+      this.owenEatsChicken()
+    }
   }
 }
